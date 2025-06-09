0b5ac44941fa2266f737c439bdcc71246bb27058
Venkata Srikanth Gullipalli <venkata.srikanthgullipalli@circana.com>
2025-06-09 18:03:03 +0530

    Local changes before pulling Github updates

--- Old_model_manual_structuredoutput.py
+++ Old_model_manual_structuredoutput.py
@@ -11,3 +11,9 @@ def custome_parser(text:str)->dict:
 parser = RunnableLambda(custome_parser)
 structured_output= parser.invoke(gpt_output)
 print(structured_output)
+
+""" When using older models like GPT-2, JSON output is not natively supported, 
+so the response defaults to plain text, this requires manual parsing either by converting 
+the text to a type dict or using tools like stroutputparse. In contrast, newer models like 
+gpt 3.5,4 support structured outputs in formats like JSON which makes parsing more seamless by using Pydantic schemas.
+"""
